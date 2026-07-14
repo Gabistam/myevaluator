e7eabc5d1b1755823cd7354fc09a90dd206e830e
Gabistam <48634603+Gabistam@users.noreply.github.com>
2026-07-14 16:39:21 +0200

    remove unnecessary React import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { DataProvider } from './contexts/DataContext';
 import { AppRouter } from './routes/AppRouter';
